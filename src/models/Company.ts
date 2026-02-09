9229f83ebfe926a066e5b4a705948e87b976a91f
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2026-02-09 21:18:07 +0100

    feat(models/Company): Add validation for phone and document

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -23,13 +23,19 @@ const CompanySchema = new Schema<CompanyDocument>(
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
+      uppercase: true,
+      trim: true,
+      match: [
+        /^([A-Z]\d{8}|\d{8}[A-Z])$/,
+        "El documento debe ser un NIF o CIF válido",
+      ],
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
     },
     address: {
       type: String,
@@ -38,6 +44,11 @@ const CompanySchema = new Schema<CompanyDocument>(
     phone: {
       type: String,
       required: true,
+      trim: true,
+      match: [
+        /^[6789]\d{8}$/,
+        "El teléfono debe ser un número válido de 9 dígitos",
+      ],
     },
     logo: {
       type: String,
