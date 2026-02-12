ae11ecb45d42b2f5a2e338a860141c25755b266d
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2026-02-12 22:52:28 +0100

    update routes and app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user";
 import companyRouter from "./routes/company";
+import mechanicRouter from "./routes/mechanic";
+import clientRouter from "./routes/client";
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/companies", companyRouter);
+app.use("/mechanics", mechanicRouter);
+app.use("/clients", clientRouter);
 
 app.use((req, res, next) => {
   return res.status(404).json("Route not found ❌");
